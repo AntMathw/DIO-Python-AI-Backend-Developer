5e83000b28ab8c139fe25542a7a58b95c700e876
Theuzin <mateusetec424@gmail.com>
2025-05-10 14:11:37 -0300

    Create convertendo_tipos.py

---
+++ 01.fundamentos/convertendo_tipos.py
@@ -0,0 +1,12 @@
+print(int(2.95435346645))
+print(int("23"))
+print(float("14.58"))
+print(float(250))
+
+valor = 23
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(23 / 2)
+print(23 // 2)
